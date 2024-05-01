07aff331963527c38192def2642ea7afcfabae95
Neven Dimač <neven.dimac@gmail.com>
2024-05-01 23:36:07 +0200

    Added SNS Publishing through the UI

---
+++ SnsPublisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using SnsPublisher;
+using System.Text.Json;
+
+var customer = new CustomerCreated()
+{
+    Id = Guid.NewGuid(),
+    Email = "neven.test@gmail.com",
+    FullName = "Neven",
+    DateOfBirth = new DateTime(1986, 1, 1),
+    GitHubUserName = "NevenD"
+};
+
+var snsClient = new AmazonSimpleNotificationServiceClient();
+
+var topicArnResponse = await snsClient.FindTopicAsync("customers");
+
+var publishRequest = new PublishRequest
+{
+    TopicArn = topicArnResponse.TopicArn,
+    Message = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(customer)
+            }
+        }
+    }
+};
+
+var response = await snsClient.PublishAsync(publishRequest);
