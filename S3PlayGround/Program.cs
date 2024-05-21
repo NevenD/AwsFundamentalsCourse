118ec129a7414d90cd96758414e91aebeb8e2f00
Neven Dimač <neven.dimac@gmail.com>
2024-05-21 20:54:25 +0200

    Read data from console application

--- S3PlayGround/Program.cs
+++ S3PlayGround/Program.cs
@@ -1,17 +1,21 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Model;
+using System.Text;
 
 var s3Client = new AmazonS3Client();
+var bucketName = "neventest";
 
-await using var inputStream = new FileStream("./face.jpg", FileMode.Open, FileAccess.Read);
 
-var putObjectRequest = new PutObjectRequest
-{
-    BucketName = "neventest",
-    Key = "images/face.jpg",
-    ContentType = "image/jpeg",
-    InputStream = inputStream
-};
+// UPDATE DATA
+//await using var inputStream = new FileStream("./face.jpg", FileMode.Open, FileAccess.Read);
+
+//var putObjectRequest = new PutObjectRequest
+//{
+//    BucketName = "neventest",
+//    Key = "images/face.jpg",
+//    ContentType = "image/jpeg",
+//    InputStream = inputStream
+//};
 
 
 //await using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
@@ -24,4 +28,18 @@ var putObjectRequest = new PutObjectRequest
 //    InputStream = inputStream
 //};
 
-await s3Client.PutObjectAsync(putObjectRequest);
+//await s3Client.PutObjectAsync(putObjectRequest);
+
+// READ DATA
+var getObjectRequest = new GetObjectRequest
+{
+    BucketName = bucketName,
+    Key = "files/movies.csv",
+};
+var response = await s3Client.GetObjectAsync(getObjectRequest);
+using var memoryStream = new MemoryStream();
+response.ResponseStream.CopyTo(memoryStream);
+
+var text = Encoding.Default.GetString(memoryStream.ToArray());
+
+Console.WriteLine(text);
